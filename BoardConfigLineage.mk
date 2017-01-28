1d692d03054421512481a8d1f377a9dc54331981
KangDroid <hyunwoo286@gmail.com>
2017-01-28 15:23:34 +0900

    Change directory

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -21,4 +21,4 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
--include vendor/google/marlin/BoardConfigVendor.mk
+-include vendor/google_devices/marlin/BoardConfigVendor.mk
